89108a2d55013935f96186c85ad8c365f6a46e16
Jenny Chu <JennyChu@Jennys-MacBook-Pro.local>
2017-01-31 20:03:03 -0800

    completed game

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -1,73 +1,108 @@
 var possibleAns = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var currentAns = "";
-var userGuess = "";
-var guessesRemaining = "";
+var currentAns;
+var userGuess;
+var guessesRemaining;
 var guessed = [];
+var dupeGuess;
+var losesCount;
+var winsCount;
 
 function newGame () {
 	//generates new answer
 	currentAns = possibleAns[Math.floor(Math.random() * possibleAns.length)];
 	console.log("Current answer: " + currentAns);
 	//reset guesses left and already guessed
+	guessed = [];
 	document.getElementById("guessesLeft").innerHTML = 10;
 	document.getElementById("alreadyGuessed").innerHTML = "";
 }
 
+function winGame() {
+	// get current win count
+	winsCount = Number(document.getElementById("winsCount").innerHTML);
+	console.log("Previous wins: " + winsCount);
+    
+    // add a win to the count
+	winsCount += 1;
+	console.log("New wins: " + winsCount);
+    
+    // display the new wins count
+	document.getElementById("winsCount").innerHTML = winsCount;
+    
+    // start new game
+	newGame();
+}
+
+function loseGame() {
+    // get current loss count
+	losesCount = Number(document.getElementById("losesCount").innerHTML);
+    
+    // increase lose count when user loses
+	losesCount += 1;
+	document.getElementById("losesCount").innerHTML = losesCount;
+    
+    // reset already guessed
+	document.getElementById("alreadyGuessed").innerHTML = "";
+    
+    // start new game
+	newGame();
+}
+
+function checkGuessedArr() {
+   // log through the 'guessed' array to see if the current guess matches any previous guesses
+    if (guessed.length == 0) {
+        console.log("checkGuessedArrIF = false");
+        dupeGuess = false;
+    }   else {
+        for (i = 0; i < guessed.length; i++) {
+            if (userGuess === guessed[i]) {
+                console.log("checkGuessedArr = true");
+                dupeGuess = true;
+            } else {
+                dupeGuess = false;
+                console.log("checkGuessedArr = false");
+            }
+        }
+    }
+}
+
 function wrongGuess () {
-	//display guesses
-	document.getElementById("alreadyGuessed").innerHTML += userGuess + " ";
-	// var listGuesses = String(document.getElementById("alreadyGuessed").innerHTML);
-	// listGuesses += userGuess + " ";
-	// document.getElementById("alreadyGuessed").innerHTML = listGuesses;
-	//decrease guesses left
-	guessesRemaining = Number(document.getElementById("guessesLeft").innerHTML);
-	guessesRemaining -= 1
-	document.getElementById("guessesLeft").innerHTML = guessesRemaining;
+	// check if guess already exists in array
+	if (dupeGuess === false) {
+		guessesRemaining = Number(document.getElementById("guessesLeft").innerHTML);
+		guessesRemaining -= 1
+		document.getElementById("guessesLeft").innerHTML = guessesRemaining;
+	}
+
+	// display guesses in browser
+	if (dupeGuess === false) {
+		document.getElementById("alreadyGuessed").innerHTML += userGuess + ", ";
+	}
+
+	// lose if no more guesses left
 	if (guessesRemaining === 0) {
 		loseGame ();
 	}
 }
 
-function winGame () {
-	//increase wins count
-	var winsCount = Number(document.getElementById("winsCount").innerHTML);
-	console.log("Previous wins: " + winsCount);
-	winsCount += 1;
-	console.log("New wins: " + winsCount);
-	document.getElementById("winsCount").innerHTML = winsCount;
-	newGame ();
-}
-
-function loseGame () {
-	var losesCount = Number(document.getElementById("losesCount").innerHTML);
-	losesCount += 1;
-	document.getElementById("losesCount").innerHTML = losesCount;
-	document.getElementById("alreadyGuessed").innerHTML = "";
-	newGame ();
-}
-
-function checkGuessedArr () {
-	for ( i = 0; i < guessed.length; i++) {
-		if (event.key === guessed[i]) {
-			return true;
-			console.log("already guessed this letter")
-		} else {
-		}
-	}
-}
-
 window.onkeydown = function(event) {
-	checkGuessedArr();
-	if (event.keyCode >= 65 && event.keyCode <= 90 && checkGuessedArr === false) {
+	if (event.keyCode >= 65 && event.keyCode <= 90) {
 		userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 		console.log("User guess is: " + userGuess);
-		//need to write this as if statment - if userGuess does not already exist in array
-		guessed.push(userGuess);
+		// add userGuess to array if did not guess yet
+		checkGuessedArr();
+		if (dupeGuess === false) {
+			guessed.push(userGuess);
+			console.log(guessed);
+		} else { }
+		
+		// check win game or wrong guess
 		if (userGuess === currentAns) {
 			winGame();
 		} else {
 			wrongGuess();
 		}
-	} else {
 	}
 }
+
+newGame();
